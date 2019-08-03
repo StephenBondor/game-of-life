6a0f7c0a92357e7fbd0a995cf9189c5759b3b06a
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-08-02 23:43:18 -0700

    Refactor to remove lines

--- gol/src/App.js
+++ gol/src/App.js
@@ -1,35 +1,25 @@
 import React, {useState, useEffect, useRef} from 'react';
-import styled from 'styled-components';
 import {parse} from 'query-string';
-// Environment Values
-const {floor, random, sqrt} = Math;
-const {innerHeight, innerWidth, location} = window;
+// Environment Values and Setup Values
+const {floor: fl, random: r, sqrt} = Math;
+const {innerHeight: iH, innerWidth: iW, location} = window;
 const {cellcount, delay} = parse(location.search);
-const size = floor(sqrt((innerHeight * innerWidth) / (cellcount || 1000)));
-const [rows, cols] = [floor(innerHeight / size), floor(innerWidth / size)];
-const [pW, pH] = [floor(innerWidth / cols), floor(innerHeight / rows)];
-const [t, f, w, h] = [true, false, pW * cols, pH * rows];
-let [ctx, cells] = [{}, [...Array(rows)].map(() => [...Array(cols)])];
-cells = cells.map(r => r.map(() => floor(random() * 2)));
-const cNeighbors = cells.map((r, i) =>
-	r.map((_, j) => {
-		let neighbors = [];
-		for (let dir = 0; dir < 9; dir++) {
-			let [n_r, n_c] = [i + ((dir % 3) - 1), j + (floor(dir / 3) - 1)];
-			if (dir !== 4 && n_r >= 0 && n_r < rows && n_c >= 0 && n_c < cols)
-				neighbors.push({x: n_r, y: n_c});
-		}
-		return neighbors;
-	})
+const size = fl(sqrt((iH * iW) / (cellcount || 1000)));
+const [R, C] = [fl(iH / size), fl(iW / size)];
+const [pW, pH, bg] = [fl(iW / C), fl(iH / R), 'background'];
+let [t, f, w, h, ctx] = [true, false, pW * C, pH * R, {}];
+let cells = [...Array(R)].map(() => [...Array(C)].map(() => fl(r() * 2)));
+const neighbCount = cells.map((r, i) =>
+	r.map((_, j) =>
+		[0, 1, 2, 3, 5, 6, 7, 8].reduce((z, d) => {
+			let [r, c, v] = [i + (d % 3), j + fl(d / 3), i + (d % 3) <= R];
+			return r && v && c && c <= C ? [...z, {x: r - 1, y: c - 1}] : z;
+		}, [])
+	)
 );
 // Styles
-const Button = styled.button`
-	width: 50px;
-	height: 50px;
-	position: fixed;
-	top: 25px;
-	left: ${innerWidth - 75}px;
-`;
+const s1 = {position: 'fixed', top: '25px', left: `${iW - 75}px`};
+const [s, E, I] = [{...s1, width: '50px', height: '50px'}, 'red', 'green'];
 const fillCell = (x, y, fill) => {
 	ctx.fillStyle = fill ? 'black' : 'white';
 	ctx.fillRect(x * pW, y * pH, pW, pH);
@@ -38,27 +28,27 @@ const fillCell = (x, y, fill) => {
 const tick = () =>
 	(cells = cells.map((r, i) =>
 		r.map((state, j) => {
-			let n = cNeighbors[i][j].reduce((a, {x, y}) => a + cells[x][y], 0); // neighbor count
+			let n = neighbCount[i][j].reduce((a, {x, y}) => a + cells[x][y], 0);
 			let alive = state ? (n < 4 ? [f, f, t, t][n] : f) : n === 3; // rules
 			if (alive !== state) fillCell(j, i, alive);
 			return alive;
 		})
 	));
 const tog = ({clientX, clientY}) => {
-	let [r, c] = [floor((clientY - 10) / pH), floor((clientX - 10) / pW)];
+	let [r, c] = [fl((clientY - 1) / pH), fl((clientX - 1) / pW)];
 	if (c >= 0 && r >= 0) fillCell(c, r, (cells[r][c] = !cells[r][c]));
 };
 // User Interface
 export default () => {
-	const [[go, setGo], cRef] = [useState(true), useRef(null)];
+	const [[g, G], cRef] = [useState(true), useRef(null)];
 	useEffect(() => {
 		ctx = cRef.current.getContext('2d');
-		let id = setInterval(() => go && tick(), delay || 100);
+		let id = setInterval(() => g && tick(), delay || 100);
 		return () => clearInterval(id);
-	}, [go, cRef]);
+	}, [g, cRef]);
 	return (
 		<>
-			<Button onClick={() => setGo(!go)}>{go ? '| |' : '▶'}</Button>
+			<button style={{...s, [bg]: g ? E : I}} onClick={() => G(!g)} />
 			<canvas ref={cRef} width={w} height={h} onClick={e => tog(e)} />
 		</>
 	);
